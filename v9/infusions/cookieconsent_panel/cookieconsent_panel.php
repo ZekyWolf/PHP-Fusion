51aa3b5d9ae446c188e68e3979ee9506e90afd71
Zeky Wolf <tomepa1925@gmail.com>
2021-05-07 20:59:03 +0200

    Cookie panel update
    Signed-off-by: Zeky Wolf <tomepa1925@gmail.com>

--- v9/infusions/cookieconsent_panel/cookieconsent_panel.php
+++ v9/infusions/cookieconsent_panel/cookieconsent_panel.php
@@ -1,17 +1,17 @@
 <?php
-/*-------------------------------------------------------+
-| PHP-Fusion Content Management System
-| Copyright (C) 2002 - 2014 Nick Jones
-| http://www.php-fusion.co.uk/
-+--------------------------------------------------------+
-| This program is released as free software under the
-| Affero GPL license. You can redistribute it and/or
-| modify it under the terms of this license which you
-| can read by viewing the included agpl.txt or online
-| at www.gnu.org/licenses/agpl.html. Removal of this
-| copyright header is strictly prohibited without
-| written permission from the original author(s).
-+--------------------------------------------------------*/
-if (!defined("IN_FUSION")) { die("Access Denied"); }
-echo "<script type='text/javascript' src='".INFUSIONS."cookieconsent_panel/js/cookieconsent.js' ></script>";
-?>
+    /*-------------------------------------------------------+
+    | PHP-Fusion Content Management System
+    | Copyright (C) 2002 - 2014 Nick Jones
+    | http://www.php-fusion.co.uk/
+    +--------------------------------------------------------+
+    | This program is released as free software under the
+    | Affero GPL license. You can redistribute it and/or
+    | modify it under the terms of this license which you
+    | can read by viewing the included agpl.txt or online
+    | at www.gnu.org/licenses/agpl.html. Removal of this
+    | copyright header is strictly prohibited without
+    | written permission from the original author(s).
+    +--------------------------------------------------------*/
+    defined('IN_FUSION') || exit;
+    
+    echo "<script type='text/javascript' src='".INFUSIONS."cookieconsent_panel/js/cookieconsent.js' ></script>";
