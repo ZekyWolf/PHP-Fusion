daf4bcd867202d175bf1fa7ce2160992eb0000a9
ZekyWolf <75901263+ZekyWolf@users.noreply.github.com>
2021-05-07 19:08:22 +0200

    Delete v8/infusion/Admin Team directory

--- v8/infusion/Admin Team/locale/English/permalinks/team.php
+++
@@ -1,3 +0,0 @@
-<?php
-$locale['pl_team_title'] = "Admin Team";
-$locale['pl_team_desc'] = "Enable Permalinks for the Team on the site.";
--- v8/infusion/Admin Team/permalinks/team_rewrite_info.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined("IN_FUSION")) {
-    die("Access Denied");
-}
-$permalink_name = $locale['pl_team_title'];
-$permalink_desc = $locale['pl_team_desc'];
